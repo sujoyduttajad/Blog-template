42fe3d12df3df3be13605d6dff34132c73bc1790
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 17:20:54 +0530

    Create ArticleList.js

---
+++ src/components/ArticleList.js
@@ -0,0 +1,105 @@
+import React from 'react'
+
+const ArticleList = () => {
+  return (
+    <div class="blog-part right-blog">
+        <marquee width="100%" direction="left">
+          <span>Now And Then You Miss It Sounds Make You Cry</span>
+          <span>Now In - MoMa Sharing Exhibition NOW</span>
+          <span>NYC Opens After Long Lockdown Check</span>
+        </marquee>
+        <div class="blog-right-title-container">
+          <div class="blog-right-title">Featured Articles</div>
+          <div class="blog-menu rounded">See All</div>
+        </div>
+        <div class="blog-right">
+          <div class="blog-right-container">
+            <div class="blog-title-date">
+              <div class="blog-right-page">1</div>
+              <div class="date">12.06.2021</div>
+            </div>
+            <div class="blog-right-page-title">Blonde - Widespread Acclaim</div>
+            <div class="blog-right-page-subtitle">
+              Blonde received widespread acclaim, with critics praising Ocean's
+              introspective lyrics and the album's
+            </div>
+          </div>
+          <div class="blog-right-container">
+            <div class="blog-title-date">
+              <div class="blog-right-page">2</div>
+              <div class="date">12.06.2021</div>
+            </div>
+            <div class="blog-right-page-title">
+              Introspective Lyrics and Beats
+            </div>
+            <div class="blog-right-page-subtitle">
+              When we toured Scotland we stopped at several selft-sealing hpuses
+              because hotels would
+            </div>
+          </div>
+          <div class="blog-right-container">
+            <div class="blog-title-date">
+              <div class="blog-right-page">3</div>
+              <div class="date">12.06.2021</div>
+            </div>
+            <div class="blog-right-page-title">
+              The Language Of Gris: Comples Beauty Of Monochrome
+            </div>
+            <div class="blog-right-page-subtitle">
+              The interior concept was conceived of by Dutch archtitect Studio
+              Anne Holtrop who cleverly emulated
+            </div>
+          </div>
+          <div class="blog-right-container">
+            <div class="blog-title-date">
+              <div class="blog-right-page">4</div>
+              <div class="date">12.06.2021</div>
+            </div>
+            <div class="blog-right-page-title">
+              A24 IS LAUNCHING ITS OWN BEAUTY BRAND
+            </div>
+            <div class="blog-right-page-subtitle">
+              Blonde received widespread acclaim, with critics praising Ocean's
+              introspective lyrics and the album's
+            </div>
+          </div>
+          <div class="blog-right-container">
+            <div class="blog-title-date">
+              <div class="blog-right-page">5</div>
+              <div class="date">12.06.2021</div>
+            </div>
+            <div class="blog-right-page-title">
+              Elon Musk's SpaceX is launching a moon satellite
+            </div>
+            <div class="blog-right-page-subtitle">
+              The interior concept was conceived of by Dutch archtitect Studio
+              Anne Holtrop who cleverly emulated
+            </div>
+          </div>
+          <div class="blog-right-container">
+            <div class="blog-title-date">
+              <div class="blog-right-page">6</div>
+              <div class="date">12.06.2021</div>
+            </div>
+            <div class="blog-right-page-title">
+              What Happens When You Leave Your Old life Behind
+            </div>
+            <div class="blog-right-page-subtitle">
+              The interior concept was conceived of by Dutch archtitect Studio
+              Anne Holtrop who cleverly emulated
+            </div>
+          </div>
+          <div class="circle">
+            <div class="circle-title">Leave Your Old Life Behind</div>
+            <div class="circle-subtitle">
+              Don't try to be like someone else, be yourself. Be secure with
+              yourself.
+            </div>
+            <div class="circle-footer">Explore</div>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default ArticleList
