a51b1f7cbd90299352622bb30e21fe4247170105
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 19:54:13 +0530

    Update MainFeed.js

--- src/components/MainFeed.js
+++ src/components/MainFeed.js
@@ -3,17 +3,17 @@ import format from 'date-fns/format';
 
 const MainFeed = () => {
   return (
-    <div class="blog-header-container">
-        <div class="blog-header">
-          <div class="blog-article header-article">
-            <div class="blog-big__title">Esteem</div>
-            <div class="blog-menu small-title date">
+    <div className="blog-header-container">
+        <div className="blog-header">
+          <div className="blog-article header-article">
+            <div className="blog-big__title">Esteem</div>
+            <div className="blog-menu small-title date">
                 {
                     format(new Date(), 'dd.MM.yyyy')
                 }
             </div>
           </div>
-          <div class="blog-article">
+          <div className="blog-article">
             <img
               src="https://images.unsplash.com/photo-1496629062893-b0f566065d44?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGJsYWNrJTIwYW5kJTIwd2hpdGV8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
               alt=""
@@ -21,7 +21,7 @@ const MainFeed = () => {
             <h2>
               Sharing The <span>Widespread</span> Acclaim About Motivation
             </h2>
-            <div class="blog-detail">
+            <div className="blog-detail">
               <span>By Richard Carnation</span>
               <span>5 Min Read</span>
             </div>
@@ -37,7 +37,7 @@ const MainFeed = () => {
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="feather feather-corner-down-right"
+                className="feather feather-corner-down-right"
                 viewBox="0 0 24 24"
               >
                 <path d="M15 10l5 5-5 5" />
@@ -47,12 +47,12 @@ const MainFeed = () => {
             </a>
           </div>
         </div>
-        <div class="blog-header">
-          <div class="blog-article header-article">
-            <div class="blog-big__title">Love</div>
-            <div class="blog-menu small-title date">12.06.2021</div>
+        <div className="blog-header">
+          <div className="blog-article header-article">
+            <div className="blog-big__title">Love</div>
+            <div className="blog-menu small-title date">12.06.2021</div>
           </div>
-          <div class="blog-article">
+          <div className="blog-article">
             <img
               src="https://images.unsplash.com/photo-1529255484355-cb73c33c04bb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTh8fGJsYWNrJTIwYW5kJTIwd2hpdGV8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
               alt=""
@@ -60,7 +60,7 @@ const MainFeed = () => {
             <h2>
               Talking About <span>Vulnerability</span> During Quarantine
             </h2>
-            <div class="blog-detail">
+            <div className="blog-detail">
               <span>By Tom Hiddleston</span>
               <span>5 Min Read</span>
             </div>
@@ -76,7 +76,7 @@ const MainFeed = () => {
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="feather feather-corner-down-right"
+                className="feather feather-corner-down-right"
                 viewBox="0 0 24 24"
               >
                 <path d="M15 10l5 5-5 5" />
@@ -86,12 +86,12 @@ const MainFeed = () => {
             </a>
           </div>
         </div>
-        <div class="blog-header">
-          <div class="blog-article header-article">
-            <div class="blog-big__title">Control</div>
-            <div class="blog-menu small-title date">12.06.2021</div>
+        <div className="blog-header">
+          <div className="blog-article header-article">
+            <div className="blog-big__title">Control</div>
+            <div className="blog-menu small-title date">12.06.2021</div>
           </div>
-          <div class="blog-article">
+          <div className="blog-article">
             <img
               src="https://images.unsplash.com/photo-1616248249518-b16013cd4e42?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTkzfHxibGFjayUyMGFuZCUyMHdoaXRlfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
               alt=""
@@ -99,7 +99,7 @@ const MainFeed = () => {
             <h2>
               How 2020 Changed <span>Understanding</span> Of Mental Health
             </h2>
-            <div class="blog-detail">
+            <div className="blog-detail">
               <span>By Scarlett Witch</span>
               <span>5 Min Read</span>
             </div>
@@ -115,7 +115,7 @@ const MainFeed = () => {
                 stroke-width="2"
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                class="feather feather-corner-down-right"
+                className="feather feather-corner-down-right"
                 viewBox="0 0 24 24"
               >
                 <path d="M15 10l5 5-5 5" />
