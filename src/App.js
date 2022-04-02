021cc92d4fa6d21b0542587e3a7dc1e33cb57e6d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 20:13:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Home from "./components/Home";
 import "./styles/global.scss";
 
 require('dotenv').config()
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const getNews = async () => {
+      const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${process.env.REACT_APP_API_KEY}`);
+      const resToJson = await response.json();
+      console.log(resToJson);
+      setData(resToJson)
+    }
+
+    getNews();
+  }, [])
+
   return (
     <>
-      <Home />
+      <Home data={data}/>
     </>
   );
 }
