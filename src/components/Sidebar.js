6ec1301d9dda62d47e6747d202aa67c1df051ed7
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 17:14:39 +0530

    Create Sidebar.js

---
+++ src/components/Sidebar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div class="blog-part is-menu">
+        <a href="#" class="blog-menu">
+          Work
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-width=".7"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-arrow-up-right"
+            viewBox="0 0 24 24"
+          >
+            <path d="M7 17L17 7M7 7h10v10" />
+          </svg>
+        </a>
+        <a href="#" class="blog-menu">
+          Studio
+        </a>
+        <a href="#" class="blog-menu">
+          Blog
+        </a>
+        <a href="#" class="blog-menu">
+          Contact
+        </a>
+        <a href="#" class="blog-menu mention">
+          @MagazineDope
+        </a>
+        <a href="#" class="blog-menu subscribe">
+          Subscribe
+        </a>
+      </div>
+  )
+}
+
+export default Sidebar
