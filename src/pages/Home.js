b0dc8144161ab14ed15bc7e8fc47022be1265c64
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 20:48:43 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,14 +10,7 @@ const Home = ({ data }) => {
   return (
     <div className="blog">
       <Sidebar/>
-      {/* <div className="blog-header blog-is-sticky">
-        <div className="blog-article header-article">
-          <div className="blog-big__title">Self</div>
-          <div className="blog-menu rounded small-title">Pinned Issue</div>
-        </div>
-        <div className="blog-article page-number">NO. 01</div>
-      </div> */}
-      <MainFeed/>
+      <MainFeed articles={data}/>
       <ArticleList/>
     </div>
   );
