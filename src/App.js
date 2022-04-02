aebab365995e2f323060be4f4cbb176927993ab6
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 20:14:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import "./styles/global.scss";
 
 require('dotenv').config()
