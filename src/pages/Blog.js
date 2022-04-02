9ed2bcd76fdfbb103cace92cde07ad08dccf609f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 20:16:35 +0530

    Create Blog.js

---
+++ src/pages/Blog.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ArticleList from "../components/ArticleList";
+import MainFeed from "../components/MainFeed";
+import Sidebar from "../components/Sidebar";
+
+const Blog = () => {
+
+  // For now it is a static site
+
+  return (
+    <div className="blog">
+      <Sidebar/>
+      <div className="blog-header blog-is-sticky">
+        <div className="blog-article header-article">
+          <div className="blog-big__title">Self</div>
+          <div className="blog-menu rounded small-title">Pinned Issue</div>
+        </div>
+        <div className="blog-article page-number">NO. 01</div>
+      </div>
+      <MainFeed/>
+      <ArticleList/>
+    </div>
+  );
+};
+
+export default Blog;
