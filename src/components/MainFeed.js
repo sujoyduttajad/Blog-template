33931a669c75e51349c98d6a76889595ece94254
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 20:48:47 +0530

    Update MainFeed.js

--- src/components/MainFeed.js
+++ src/components/MainFeed.js
@@ -1,54 +1,58 @@
-import React from 'react'
-import format from 'date-fns/format';
+import React from "react";
+import format from "date-fns/format";
 import { Link } from "react-router-dom";
 
-const MainFeed = () => {
+const MainFeed = ({ articles }) => {
   return (
     <div className="blog-header-container">
-        <div className="blog-header">
-          <div className="blog-article header-article">
-            <div className="blog-big__title">Esteem</div>
-            <div className="blog-menu small-title date">
-                {
-                    format(new Date(), 'dd.MM.yyyy')
-                }
+        <div className="blog-big__title">MagazineDope</div>
+      {articles.map((article) => {
+        return (
+          <div className="blog-header">
+            <div className="blog-article header-article">
+            {/* <div className="blog-big__title">Esteem</div> */}
+              <div className="blog-menu small-title date">
+                {format(new Date(), "dd.MM.yyyy")}
+              </div>
+            </div>
+            <div className="blog-article">
+              <img
+                src="https://images.unsplash.com/photo-1496629062893-b0f566065d44?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGJsYWNrJTIwYW5kJTIwd2hpdGV8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                alt=""
+              />
+              <h2>
+                Sharing The <span>Widespread</span> Acclaim About Motivation
+              </h2>
+              <div className="blog-detail">
+                <span>By Richard Carnation</span>
+                <span>5 Min Read</span>
+              </div>
+              <p>
+                Blonde received widespread acclaim, with critics praising
+                Ocean's introspective lyrics and the album's unconventional
+              </p>
+              <Link to="#">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="feather feather-corner-down-right"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M15 10l5 5-5 5" />
+                  <path d="M4 4v7a4 4 0 004 4h12" />
+                </svg>
+                See More
+              </Link>
             </div>
           </div>
-          <div className="blog-article">
-            <img
-              src="https://images.unsplash.com/photo-1496629062893-b0f566065d44?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGJsYWNrJTIwYW5kJTIwd2hpdGV8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-              alt=""
-            />
-            <h2>
-              Sharing The <span>Widespread</span> Acclaim About Motivation
-            </h2>
-            <div className="blog-detail">
-              <span>By Richard Carnation</span>
-              <span>5 Min Read</span>
-            </div>
-            <p>
-              Blonde received widespread acclaim, with critics praising Ocean's
-              introspective lyrics and the album's unconventional
-            </p>
-            <Link to="#">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                className="feather feather-corner-down-right"
-                viewBox="0 0 24 24"
-              >
-                <path d="M15 10l5 5-5 5" />
-                <path d="M4 4v7a4 4 0 004 4h12" />
-              </svg>
-              See More
-            </Link>
-          </div>
-        </div>
-        <div className="blog-header">
+        );
+      })}
+
+      {/* <div className="blog-header">
           <div className="blog-article header-article">
             <div className="blog-big__title">Love</div>
             <div className="blog-menu small-title date">12.06.2021</div>
@@ -125,9 +129,9 @@ const MainFeed = () => {
               See More
             </Link>
           </div>
-        </div>
-      </div>
-  )
-}
+        </div> */}
+    </div>
+  );
+};
 
-export default MainFeed
+export default MainFeed;
