7f3c4335b004f887a6bfa577a72672ecccdb80fc
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 17:14:41 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,10 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
 const Home = () => {
   return (
     <div class="blog">
-      <div class="blog-part is-menu">
-        <a href="#" class="blog-menu">
-          Work
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-width=".7"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="feather feather-arrow-up-right"
-            viewBox="0 0 24 24"
-          >
-            <path d="M7 17L17 7M7 7h10v10" />
-          </svg>
-        </a>
-        <a href="#" class="blog-menu">
-          Studio
-        </a>
-        <a href="#" class="blog-menu">
-          Blog
-        </a>
-        <a href="#" class="blog-menu">
-          Contact
-        </a>
-        <a href="#" class="blog-menu mention">
-          @MagazineDope
-        </a>
-        <a href="#" class="blog-menu subscribe">
-          Subscribe
-        </a>
-      </div>
+      <Sidebar/>
       <div class="blog-header blog-is-sticky">
         <div class="blog-article header-article">
           <div class="blog-big__title">Self</div>
