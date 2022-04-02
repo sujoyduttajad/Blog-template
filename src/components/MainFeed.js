3795b50e63bcdb27aabef7782db7c30f34a9c1fa
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-04-02 20:53:31 +0530

    Update MainFeed.js

--- src/components/MainFeed.js
+++ src/components/MainFeed.js
@@ -36,9 +36,9 @@ const MainFeed = ({ articles }) => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   className="feather feather-corner-down-right"
                   viewBox="0 0 24 24"
                 >
